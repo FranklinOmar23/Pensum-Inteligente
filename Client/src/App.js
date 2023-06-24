8863d1a52633333f3fa2300f845557883c179f8c
AngelGarciaCabrera <122624789+AngelGarciaCabrera@users.noreply.github.com>
2023-06-24 17:56:15 -0500

    correcion de merge

--- Client/src/App.js
+++ Client/src/App.js
@@ -6,6 +6,7 @@ import React from "react";
 import {UserHome} from "./components/UserHome";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 function App() {
     return (
         <div className="App">
