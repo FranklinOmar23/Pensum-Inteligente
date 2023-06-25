b10597dbf1b2a675d9cddf1289f66086d44e8f87
Angelo Vargas <115881245+CompreAngelo@users.noreply.github.com>
2023-06-25 19:25:57 -0400

    Creacion de vista Home combinada con el Registro

--- Client/src/Components/Compo_Helpers/InputContra.jsx
+++ Client/src/Components/Compo_Helpers/InputContra.jsx
@@ -5,7 +5,7 @@ function InputContra ({label}){
     return(
         <Form.Group className="mb-3" controlId="formBasicPassword">
       <div className="form-control-container">
-        <Form.Control plaintext className="effect-2 custom-input" type="password" placeholder={label} style={{ width: '400px', color: '#DAFFFB' }}/>
+        <Form.Control plaintext id={label} className="effect-2 custom-input" type="password" placeholder={label}  required  style={{ width: '400px', color: '#DAFFFB' }}/>
         <div className="focus-border"></div>
       </div>
     </Form.Group>
