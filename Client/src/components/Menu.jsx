8c5da136f72adf73427ba98ca228fbae69a821be
AngelGarciaCabrera <122624789+AngelGarciaCabrera@users.noreply.github.com>
2023-06-25 00:42:42 -0500

    terminacion del primer home

--- Client/src/components/Menu.jsx
+++ Client/src/components/Menu.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import '../styles.css';
 import { LI } from "./Comp_helpers/LI";
+import logo from "../logo_Mesa de trabajo 1.png"
+
 
 export function Menu() {
+
   const [showOptions, setShowOptions] = useState(false);
 
   const toggleOptions = () => {
@@ -13,6 +16,7 @@ export function Menu() {
     <>
       <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
         <div className="container px-4 px-lg-5">
+          <a className="navbar-brand"><img src={logo} className="logo"/> </a>
           <a className="navbar-brand" href="#page-top">Your Pensum</a>
           <button
             className={`navbar-toggler navbar-toggler-right ${showOptions ? 'open' : ''}`}
