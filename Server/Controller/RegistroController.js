2cdc8e789147fdb757b64c410c8e215b1c933b8e
Angelo Vargas <115881245+CompreAngelo@users.noreply.github.com>
2023-07-05 21:00:29 -0400

    Cambios al Controlador de Registro

--- Server/Controller/RegistroController.js
+++ Server/Controller/RegistroController.js
@@ -1,22 +1,42 @@
 import { getConnection } from "../DataBase/Conection.js";
 import Estudiante from '../Models/Usuario.js';
 
-async function registroController(nombre, email, password) {
+async function registroController(Nombre, Email, Contraseña) {
   try {
     const pool = getConnection(); // Obtener el pool de conexiones
 
-    const estudiante = new Estudiante(null, nombre, email, password, null, null, null, null);
+    const estudiante = new Estudiante(null, Nombre, Email, Contraseña, null, null, null, null);
 
-    const query = "INSERT INTO estudiante (Nombre, Email, Contraseña) VALUES (?, ?, ?)";
-    const values = [estudiante.getNombre(), estudiante.getEmail(), estudiante.getContraseña()];
+    // Verificar si el correo electrónico ya está registrado
+    const checkQuery = "SELECT COUNT(*) AS count FROM estudiante WHERE Email = ?";
+    const checkValues = [estudiante.getEmail()];
 
-    await new Promise((resolve, reject) => {
-      pool.query(query, values, (error, results, fields) => {
+    const checkResult = await new Promise((resolve, reject) => {
+      pool.query(checkQuery, checkValues, (error, results, fields) => {
         if (error) {
           reject(error);
           return;
         }
-        console.log("Estudiante insertado correctamente");
+        resolve(results[0].count > 0); // Devuelve true si el correo electrónico ya está registrado
+      });
+    });
+
+    if (checkResult) {
+      console.warn("El correo electrónico ya está registrado. No se puede agregar.");
+      return;
+    }
+
+    // Insertar el estudiante si el correo electrónico no está registrado
+    const insertQuery = "INSERT INTO estudiante (Nombre, Email, Contraseña) VALUES (?, ?, ?)";
+    const insertValues = [estudiante.getNombre(), estudiante.getEmail(), estudiante.getContraseña()];
+
+    await new Promise((resolve, reject) => {
+      pool.query(insertQuery, insertValues, (error, results, fields) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        console.warn("Estudiante insertado correctamente");
         resolve();
       });
     });
